3c79b1fa867c229647fd104fb1ece45ddd5eb620
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2025-02-16 18:20:05 +0600

    wip: issue#3 - tenants dependency installed
    tenant app created

---
+++ settings/env.py
@@ -0,0 +1,23 @@
+# Database Settings
+DATABASES_SETTINGS = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_name',
+        'USER': 'db_username',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+ALLOWED_HOSTS_SETTINGS = []
+
+SECRET_KEY_SETTINGS = 'projectssecretekey'
+CORS_ALLOWED_ORIGINS_SETTINGS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:8080",
+]
+
+X_FRAME_OPTIONS_SETTINGS = 'ALLOW-FROM http://localhost:8080'
+
+FRONTEND_BASE_URL_SETTINGS = ''
